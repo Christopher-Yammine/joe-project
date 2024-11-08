f7cb1b678856acd16975fc58d83138816e9dd927
Joe Haddad <joehaddad94@gmail.com>
2024-11-08 17:57:46 +0200

    Edit <> Edited demographic and etl seeder

--- obsrvr-api/database/seeders/DemographicsSeeder.php
+++ obsrvr-api/database/seeders/DemographicsSeeder.php
@@ -1,44 +1,82 @@
 <?php
 
+// namespace Database\Seeders;
+
+// use Illuminate\Database\Seeder;
+// use App\Models\Demographic;
+// use App\Models\PersonType;
+// use App\Models\Gender;
+// use App\Models\Sentiment;
+// use App\Models\AgeGroup;
+// use App\Models\Stream;
+
+// class DemographicsSeeder extends Seeder
+// {
+//     public function run()
+//     {
+       
+       
+//         $genders = Gender::all()->pluck('id')->toArray();
+//         $sentiments = Sentiment::all();
+//         $ageGroups = AgeGroup::all();
+      
+
+   
+    
+//             foreach ($genders as $gender) {
+//                 foreach ($sentiments as $sentiment) {
+//                     foreach ($ageGroups as $ageGroup) {
+                       
+                           
+//                             Demographic::create([
+                               
+                               
+//                                 'gender_id' => $gender->id,
+//                                 'age_group_id' => $ageGroup->id,
+//                                 'sentiment_id' => $sentiment->id,
+//                             ]);
+                    
+//                     }
+//                 }
+//             }
+        
+//     }
+// }
+
+
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Demographic;
-use App\Models\PersonType;
 use App\Models\Gender;
 use App\Models\Sentiment;
 use App\Models\AgeGroup;
-use App\Models\Stream;
 
 class DemographicsSeeder extends Seeder
 {
     public function run()
     {
-       
-       
-        $genders = Gender::all();
-        $sentiments = Sentiment::all();
-        $ageGroups = AgeGroup::all();
-      
+        $genderIds = Gender::all()->pluck('id')->toArray();
+        $sentimentIds = Sentiment::all()->pluck('id')->toArray();
+        $ageGroupIds = AgeGroup::all()->pluck('id')->toArray();
 
-   
-    
-            foreach ($genders as $gender) {
-                foreach ($sentiments as $sentiment) {
-                    foreach ($ageGroups as $ageGroup) {
-                       
-                           
-                            Demographic::create([
-                               
-                               
-                                'gender_id' => $gender->id,
-                                'age_group_id' => $ageGroup->id,
-                                'sentiment_id' => $sentiment->id,
-                            ]);
-                    
-                    }
-                }
-            }
-        
+        $counter = 0;
+        $limit = 1000;
+
+        while ($counter < $limit) {
+            $randomGenderId = $genderIds[array_rand($genderIds)];
+            $randomAgeGroupId = $ageGroupIds[array_rand($ageGroupIds)];
+            $randomSentimentId = $sentimentIds[array_rand($sentimentIds)];
+
+            Demographic::create([
+                'gender_id' => $randomGenderId,
+                'age_group_id' => $randomAgeGroupId,
+                'sentiment_id' => $randomSentimentId,
+            ]);
+
+            $counter++;
+        }
     }
 }
+
--- obsrvr-api/database/seeders/ETLDataSeeder.php
+++ obsrvr-api/database/seeders/ETLDataSeeder.php
@@ -16,169 +16,277 @@ use App\Models\PersonType;
 use App\Models\Stream;
 use Carbon\Carbon;
 
+// class ETLDataSeeder extends Seeder
+// {
+//     public function run()
+//     {
+//         $footfalls = PersonType::all();
+//         $metrics = Metric::all();
+//         $demographics = Demographic::all();
+//         $streams = Stream::all();
+//         // $startDate = Carbon::yesterday()->setTime(0, 0, 0);
+//         // $endDateHourly = $startDate->copy()->addHours(36);
+//         // $endDateDaily = $startDate->copy()->addDays(30);
+//         // $endDateWeekly = $startDate->copy()->addWeeks(12);
+//         // $endDateMonthly = $startDate->copy()->addMonths(6);
+//         // $endDateQuarterly = $startDate->copy()->addQuarters(4);
+//         // $endDateYearly = $startDate->copy()->addYears(2);
+
+//         $startDateHourly = Carbon::yesterday()->setTime(0, 0, 0);
+//         $endDateHourly = Carbon::now();
+
+//         $startDateDaily = $startDateHourly;
+//         $endDateDaily = Carbon::now()->endOfDay();
+
+//         $startDateWeekly = Carbon::now()->startOfWeek()->subWeeks(12);
+//         $endDateWeekly = Carbon::now()->endOfWeek();
+
+//         $startDateMonthly = Carbon::now()->startOfMonth()->subMonths(6);
+//         $endDateMonthly = Carbon::now()->endOfMonth();
+
+//         $startDateQuarterly = Carbon::now()->startOfQuarter()->subQuarters(4);
+//         $endDateQuarterly = Carbon::now()->endOfQuarter();
+
+//         $startDateYearly = Carbon::now()->startOfYear()->subYears(2);
+//         $endDateYearly = Carbon::now()->endOfYear();
+
+//         $this->seedHourlyData($footfalls, $demographics, $metrics, $startDateHourly, $endDateHourly, $streams);
+//         $this->seedDailyData($footfalls, $demographics, $metrics, $startDateDaily, $endDateDaily, $streams);
+//         $this->seedWeeklyData($footfalls, $demographics, $metrics, $startDateWeekly, $endDateWeekly, $streams);
+//         $this->seedMonthlyData($footfalls, $demographics, $metrics, $startDateMonthly, $endDateMonthly, $streams);
+//         $this->seedQuarterlyData($footfalls, $demographics, $metrics, $startDateQuarterly, $endDateQuarterly, $streams);
+//         $this->seedYearlyData($footfalls, $demographics, $metrics, $startDateYearly, $endDateYearly, $streams);
+//     }
+
+//     protected function seedHourlyData($footfalls, $demographics, $metrics, $startTime, $endTime, $streams)
+//     {
+//         $demographics = $demographics->take(10);
+//         while ($startTime < $endTime) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                         foreach ($metrics as $metric) {
+//                             ETLDataHourly::create([
+//                                 'stream_id' => $stream->id,
+//                                 'person_type_id' => $footfall->id,
+//                                 'demographics_id' => $demographic->id,
+//                                 'metric_id' => $metric->id,
+//                                 'date' => $startTime->format('Y-m-d H:i:s'),
+//                                 'value' => rand(0, 10),
+//                             ]);
+//                         }
+//                     }
+//                 }
+//             }
+//             $startTime->addHour();
+//         }
+//     }
+
+//     protected function seedDailyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
+//     {
+//         while ($startDate < $endDate) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                     foreach ($metrics as $metric) {
+//                         ETLDataDaily::create([
+//                             'stream_id' => $stream->id,
+//                             'person_type_id' => $footfall->id,
+//                             'demographics_id' => $demographic->id,
+//                             'metric_id' => $metric->id,
+//                             'date' => $startDate->format('Y-m-d H:i:s'),
+//                             'value' => rand(0, 10),
+//                         ]);
+//                     }
+//                 }
+//                 }
+//             }
+//             $startDate->addDay();
+//         }
+//     }
+
+//     protected function seedWeeklyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
+//     {
+//         while ($startDate < $endDate) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                         foreach ($metrics as $metric) {
+//                             ETLDataWeekly::create([
+//                                 'stream_id'=> $stream->id,
+//                                 'person_type_id' => $footfall->id,
+//                                 'demographics_id' => $demographic->id,
+//                                 'metric_id' => $metric->id,
+//                                 'date' => $startDate->format('Y-m-d H:i:s'),
+//                                 'value' => rand(0, 10),
+//                             ]);
+//                         }
+//                     }
+//                 }
+//             }
+//             $startDate->addWeek();
+//         }
+//     }
+
+//     protected function seedMonthlyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
+//     {
+//         while ($startDate < $endDate) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                     foreach ($metrics as $metric) {
+//                         ETLDataMonthly::create([
+//                             'stream_id'=> $stream->id,
+//                             'person_type_id' => $footfall->id,
+//                             'demographics_id' => $demographic->id,
+//                             'metric_id' => $metric->id,
+//                             'date' => $startDate->format('Y-m-d H:i:s'),
+//                             'value' => rand(0, 10),
+//                         ]);
+//                     }
+//                     }
+//                 }
+//             }
+//             $startDate->addMonth();
+//         }
+//     }
+
+//     protected function seedQuarterlyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
+//     {
+//         while ($startDate < $endDate) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                         foreach ($metrics as $metric) {
+//                             ETLDataQuarterly::create([
+//                                 'stream_id'=> $stream->id,
+//                                 'person_type_id' => $footfall->id,
+//                                 'demographics_id' => $demographic->id,
+//                                 'metric_id' => $metric->id,
+//                                 'date' => $startDate->format('Y-m-d H:i:s'),
+//                                 'value' => rand(0, 10),
+//                             ]);
+//                         }
+//                     }
+//                 }
+//             }
+//             $startDate->addQuarter();
+//         }
+//     }
+
+//     protected function seedYearlyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
+//     {
+//         while ($startDate < $endDate) {
+//             foreach ($footfalls as $footfall) {
+//                 foreach ($demographics as $demographic) {
+//                     foreach ($streams as $stream) {
+//                         foreach ($metrics as $metric) {
+//                             ETLDataYearly::create([
+//                                 'stream_id'=> $stream->id,
+//                                 'person_type_id' => $footfall->id,
+//                                 'demographics_id' => $demographic->id,
+//                                 'metric_id' => $metric->id,
+//                                 'date' => $startDate->format('Y-m-d H:i:s'),
+//                                 'value' => rand(0, 10),
+//                             ]);
+//                         }
+//                     }
+//                 }
+//             }
+//             $startDate->addYear();
+//         }
+//     }
+// }
+
 class ETLDataSeeder extends Seeder
 {
     public function run()
     {
-        $footfalls = PersonType::all();
-        $metrics = Metric::all();
-        $demographics = Demographic::all();
-        $streams = Stream::all();
-        $startDate = Carbon::yesterday()->setTime(0, 0, 0);
-        $endDateHourly = $startDate->copy()->addHours(36);
-        $endDateDaily = $startDate->copy()->addDays(30);
-        $endDateWeekly = $startDate->copy()->addWeeks(12);
-        $endDateMonthly = $startDate->copy()->addMonths(6);
-        $endDateQuarterly = $startDate->copy()->addQuarters(4);
-        $endDateYearly = $startDate->copy()->addYears(2);
+        $footfalls = PersonType::all()->pluck('id')->toArray();
+        $metrics = Metric::all()->pluck('id')->toArray();
+        $demographics = Demographic::all()->take(10)->pluck('id')->toArray();
+        $streams = Stream::all()->pluck('id')->toArray();
 
-        // $startDateHourly = Carbon::yesterday()->setTime(0, 0, 0);
-        // $endDateHourly = Carbon::now();
+        $startDateHourly = Carbon::yesterday()->setTime(0, 0, 0);
+        $endDateHourly = Carbon::now();
 
-        // $startDateDaily = $startDateHourly;
-        // $endDateDaily = Carbon::now()->endOfDay();
+        $startDateDaily = $startDateHourly->copy();
+        $endDateDaily = Carbon::now()->endOfDay();
 
-        // $startDateWeekly = Carbon::now()->startOfWeek()->subWeeks(12);
-        // $endDateWeekly = Carbon::now()->endOfWeek();
+        $startDateWeekly = Carbon::now()->startOfWeek()->subWeeks(12);
+        $endDateWeekly = Carbon::now()->endOfWeek();
 
-        // $startDateMonthly = Carbon::now()->startOfMonth()->subMonths(6);
-        // $endDateMonthly = Carbon::now()->endOfMonth();
+        $startDateMonthly = Carbon::now()->startOfMonth()->subMonths(6);
+        $endDateMonthly = Carbon::now()->endOfMonth();
 
-        // $startDateQuarterly = Carbon::now()->startOfQuarter()->subQuarters(4);
-        // $endDateQuarterly = Carbon::now()->endOfQuarter();
+        $startDateQuarterly = Carbon::now()->startOfQuarter()->subQuarters(4);
+        $endDateQuarterly = Carbon::now()->endOfQuarter();
 
-        // $startDateYearly = Carbon::now()->startOfYear()->subYears(2);
-        // $endDateYearly = Carbon::now()->endOfYear();
+        $startDateYearly = Carbon::now()->startOfYear()->subYears(2);
+        $endDateYearly = Carbon::now()->endOfYear();
 
-        $this->seedHourlyData($footfalls, $demographics, $metrics, $startDate, $endDateHourly, $streams);
-        $this->seedDailyData($footfalls, $demographics, $metrics, $startDate, $endDateDaily, $streams);
-        $this->seedWeeklyData($footfalls, $demographics, $metrics, $startDate, $endDateWeekly, $streams);
-        $this->seedMonthlyData($footfalls, $demographics, $metrics, $startDate, $endDateMonthly, $streams);
-        $this->seedQuarterlyData($footfalls, $demographics, $metrics, $startDate, $endDateQuarterly, $streams);
-        $this->seedYearlyData($footfalls, $demographics, $metrics, $startDate, $endDateYearly, $streams);
+        $this->seedData(ETLDataHourly::class, $footfalls, $demographics, $metrics, $startDateHourly, $endDateHourly, $streams, 'hour');
+        $this->seedData(ETLDataDaily::class, $footfalls, $demographics, $metrics, $startDateDaily, $endDateDaily, $streams, 'day');
+        $this->seedData(ETLDataWeekly::class, $footfalls, $demographics, $metrics, $startDateWeekly, $endDateWeekly, $streams, 'week');
+        $this->seedData(ETLDataMonthly::class, $footfalls, $demographics, $metrics, $startDateMonthly, $endDateMonthly, $streams, 'month');
+        $this->seedData(ETLDataQuarterly::class, $footfalls, $demographics, $metrics, $startDateQuarterly, $endDateQuarterly, $streams, 'quarter');
+        $this->seedData(ETLDataYearly::class, $footfalls, $demographics, $metrics, $startDateYearly, $endDateYearly, $streams, 'year');
     }
 
-    protected function seedHourlyData($footfalls, $demographics, $metrics, $startTime, $endTime, $streams)
+    protected function seedData($model, $footfalls, $demographics, $metrics, $startDate, $endDate, $streams, $interval)
     {
-        $demographics = $demographics->take(10);
-        while ($startTime < $endTime) {
+        $data = [];
+        $date = $startDate->copy();
+
+        while ($date <= $endDate) {
             foreach ($footfalls as $footfall) {
                 foreach ($demographics as $demographic) {
                     foreach ($streams as $stream) {
                         foreach ($metrics as $metric) {
-                            ETLDataHourly::create([
-                                'stream_id' => $stream->id,
-                                'person_type_id' => $footfall->id,
-                                'demographics_id' => $demographic->id,
-                                'metric_id' => $metric->id,
-                                'date' => $startTime->format('Y-m-d H:i:s'),
+                            $data[] = [
+                                'stream_id' => $stream,
+                                'person_type_id' => $footfall,
+                                'demographics_id' => $demographic,
+                                'metric_id' => $metric,
+                                'date' => $date->format('Y-m-d H:i:s'),
                                 'value' => rand(0, 10),
-                            ]);
+                            ];
+
+                            if (count($data) >= 1000) {
+                                $model::insert($data);
+                                $data = [];
+                            }
                         }
                     }
                 }
             }
-            $startTime->addHour();
-        }
-    }
 
-    protected function seedDailyData($footfalls, $demographics, $metrics, $startDate, $endDate, $streams)
-    {
-        while ($startDate < $endDate) {
-            foreach ($footfalls as $footfall) {
-                foreach ($demographics as $demographic) {
-                    foreach ($metrics as $metric) {
-                        ETLDataDaily::create([
-                            'person_type_id' => $footfall->id,
-                            'demographics_id' => $demographic->id,
-                            'metric_id' => $metric->id,
-                            'date' => $startDate->format('Y-m-d H:i:s'),
-                            'value' => rand(0, 10),
-                        ]);
-                    }
-                }
+            // Advance the date
+            switch ($interval) {
+                case 'hour':
+                    $date->addHour();
+                    break;
+                case 'day':
+                    $date->addDay();
+                    break;
+                case 'week':
+                    $date->addWeek();
+                    break;
+                case 'month':
+                    $date->addMonth();
+                    break;
+                case 'quarter':
+                    $date->addQuarter();
+                    break;
+                case 'year':
+                    $date->addYear();
+                    break;
             }
-            $startDate->addDay();
         }
-    }
 
-    protected function seedWeeklyData($footfalls, $demographics, $metrics, $startDate, $endDate)
-    {
-        while ($startDate < $endDate) {
-            foreach ($footfalls as $footfall) {
-                foreach ($demographics as $demographic) {
-                    foreach ($metrics as $metric) {
-                        ETLDataWeekly::create([
-                            'person_type_id' => $footfall->id,
-                            'demographics_id' => $demographic->id,
-                            'metric_id' => $metric->id,
-                            'date' => $startDate->format('Y-m-d H:i:s'),
-                            'value' => rand(0, 10),
-                        ]);
-                    }
-                }
-            }
-            $startDate->addWeek();
-        }
-    }
-
-    protected function seedMonthlyData($footfalls, $demographics, $metrics, $startDate, $endDate)
-    {
-        while ($startDate < $endDate) {
-            foreach ($footfalls as $footfall) {
-                foreach ($demographics as $demographic) {
-                    foreach ($metrics as $metric) {
-                        ETLDataMonthly::create([
-                            'person_type_id' => $footfall->id,
-                            'demographics_id' => $demographic->id,
-                            'metric_id' => $metric->id,
-                            'date' => $startDate->format('Y-m-d H:i:s'),
-                            'value' => rand(0, 10),
-                        ]);
-                    }
-                }
-            }
-            $startDate->addMonth();
-        }
-    }
-
-    protected function seedQuarterlyData($footfalls, $demographics, $metrics, $startDate, $endDate)
-    {
-        while ($startDate < $endDate) {
-            foreach ($footfalls as $footfall) {
-                foreach ($demographics as $demographic) {
-                    foreach ($metrics as $metric) {
-                        ETLDataQuarterly::create([
-                            'person_type_id' => $footfall->id,
-                            'demographics_id' => $demographic->id,
-                            'metric_id' => $metric->id,
-                            'date' => $startDate->format('Y-m-d H:i:s'),
-                            'value' => rand(0, 10),
-                        ]);
-                    }
-                }
-            }
-            $startDate->addQuarter();
-        }
-    }
-
-    protected function seedYearlyData($footfalls, $demographics, $metrics, $startDate, $endDate)
-    {
-        while ($startDate < $endDate) {
-            foreach ($footfalls as $footfall) {
-                foreach ($demographics as $demographic) {
-                    foreach ($metrics as $metric) {
-                        ETLDataYearly::create([
-                            'person_type_id' => $footfall->id,
-                            'demographics_id' => $demographic->id,
-                            'metric_id' => $metric->id,
-                            'date' => $startDate->format('Y-m-d H:i:s'),
-                            'value' => rand(0, 10),
-                        ]);
-                    }
-                }
-            }
-            $startDate->addYear();
+        // Insert any remaining data
+        if (!empty($data)) {
+            $model::insert($data);
         }
     }
 }
