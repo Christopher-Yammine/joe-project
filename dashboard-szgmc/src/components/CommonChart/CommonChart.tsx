20d9174582e9eb4ebca528ee0028ce37e77c3ecd
Joe Haddad <joehaddad94@gmail.com>
2024-11-09 00:55:01 +0200

    Edit <> Fixed percentage color

--- dashboard-szgmc/src/components/CommonChart/CommonChart.tsx
+++ dashboard-szgmc/src/components/CommonChart/CommonChart.tsx
@@ -172,81 +172,83 @@ const LineChart: React.FC<Props> = ({
     <Grid item xs={12} md={6}>
       <Card>
         <Box sx={{ p: '1.5rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '20px' }}>
-          {data.map((item: any, index: number) => (
-            <Box
-              key={index}
-              sx={{
-                py: 2,
-                px: 3,
-                display: 'flex',
-                borderRadius: 1,
-                alignItems: 'center',
-                backgroundColor: 'background.default',
-                height: '100%',
-                width: '100%'
-              }}
-            >
+          {data.map((item: any, index: number) => {
+            return (
               <Box
+                key={index}
                 sx={{
-                  width: '100%',
+                  py: 2,
+                  px: 3,
                   display: 'flex',
-                  flexWrap: 'wrap',
-                  alignItems: 'flex-end',
-                  justifyContent: 'space-between'
+                  borderRadius: 1,
+                  alignItems: 'center',
+                  backgroundColor: 'background.default',
+                  height: '100%',
+                  width: '100%'
                 }}
               >
-                <Box sx={{ mr: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Typography sx={{ color: 'text.secondary' }}>{item.title}</Typography>
-                  <Typography sx={{ fontWeight: 500, fontSize: '1.125rem' }}>{item.stats}</Typography>
-                </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
-                  <Typography
-                    variant='body2'
-                    sx={{
-                      mb: 0.5,
-                      fontWeight: 500,
-                      color: `${item.trend === 'negative' || item?.isReversed ? 'red' : 'green'}`
-                    }}
-                  >
-                    {`${item.trendNumber}%`}
-                  </Typography>
-
-                  {item.trend !== 'negative' ? (
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='10px'
-                      height='10px'
-                      fill={item.trend === 'negative' || item?.isReversed ? 'red' : 'green'}
-                      viewBox='0 0 384 512'
+                <Box
+                  sx={{
+                    width: '100%',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'flex-end',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <Box sx={{ mr: 2, display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ color: 'text.secondary' }}>{item.title}</Typography>
+                    <Typography sx={{ fontWeight: 500, fontSize: '1.125rem' }}>{item.stats}</Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
+                    <Typography
+                      variant='body2'
+                      sx={{
+                        mb: 0.5,
+                        fontWeight: 500,
+                        color: `${item?.trendNumber?.startsWith('-') || item?.isReversed ? 'red' : 'green'}`
+                      }}
                     >
-                      <path d='M32 448C14.3 448 0 462.3 0 480S14.3 512 32 512h96c53 0 96-43 96-96V109.3L297.4 182.7c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L214.7 9.4C202.2-3.1 181.8-3.1 169.3 9.4L41.3 137.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 109.3V416c0 17.7 14.3 32 32 32h96z' />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width={'10px'}
-                      height={'10px'}
-                      fill={item.trend === 'negative' || item?.isReversed ? 'red' : 'green'}
-                      viewBox='0 0 384 512'
-                    >
-                      <path d='M32 64C14.3 64 0 49.7 0 32S14.3 0 32 0l96 0c53 0 96 43 96 96l0 306.7 73.4-73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 402.7 160 96c0-17.7-14.3-32-32-32L32 64z' />
-                    </svg>
-                  )}
+                      {`${item.trendNumber}`}
+                    </Typography>
 
-                  <Typography
-                    variant='body2'
-                    sx={{
-                      mb: 0.5,
-                      fontWeight: 500,
-                      color: `${item.trend === 'negative' || item?.isReversed ? 'red' : 'green'}`
-                    }}
-                  >
-                    PW
-                  </Typography>
+                    {!item?.trendNumber?.startsWith('-') ? (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='10px'
+                        height='10px'
+                        fill={item?.trendNumber?.startsWith('-') || item?.isReversed ? 'red' : 'green'}
+                        viewBox='0 0 384 512'
+                      >
+                        <path d='M32 448C14.3 448 0 462.3 0 480S14.3 512 32 512h96c53 0 96-43 96-96V109.3L297.4 182.7c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L214.7 9.4C202.2-3.1 181.8-3.1 169.3 9.4L41.3 137.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 109.3V416c0 17.7 14.3 32 32 32h96z' />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width={'10px'}
+                        height={'10px'}
+                        fill={item?.trendNumber?.startsWith('-') || item?.isReversed ? 'red' : 'green'}
+                        viewBox='0 0 384 512'
+                      >
+                        <path d='M32 64C14.3 64 0 49.7 0 32S14.3 0 32 0l96 0c53 0 96 43 96 96l0 306.7 73.4-73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 402.7 160 96c0-17.7-14.3-32-32-32L32 64z' />
+                      </svg>
+                    )}
+
+                    <Typography
+                      variant='body2'
+                      sx={{
+                        mb: 0.5,
+                        fontWeight: 500,
+                        color: `${item?.trendNumber?.startsWith('-') || item?.isReversed ? 'red' : 'green'}`
+                      }}
+                    >
+                      PW
+                    </Typography>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          ))}
+            )
+          })}
         </Box>
         <CardContent>
           {isChartLoaded && <ReactApexcharts type='line' height={300} options={options} series={mappedSeries} />}
